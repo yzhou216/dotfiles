a665571dedc4d53e508d75a2e4d5d12af6c5124c
Yiyu Zhou <yiyuzhou19@gmail.com>
2023-10-10 00:32:48 -0700

    Add git-gutter

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -63,6 +63,18 @@
   ;; set backslash as local leader
   (evil-set-leader 'normal "\\" t))
 
+;; git-gutter
+(use-package git-gutter
+  :hook (prog-mode . git-gutter-mode)
+  :config
+  (setq git-gutter:update-interval 0.02))
+
+(use-package git-gutter-fringe
+  :config
+  (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
+
 ;; which-key
 (use-package which-key
   :config
