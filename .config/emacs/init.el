cd7fbc15f91b961d8fd7369545332a10d4561839
Yiyu Zhou <yiyuzhou19@gmail.com>
2024-08-11 19:39:05 -0700

    Change Company mode to Corfu

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -122,8 +122,26 @@ source: https://www.emacswiki.org/emacs/FlySpell "
           (lambda ()
             (eshell/alias "clear" "clear 1")))
 
-;; use-package
 (require 'use-package)
+
+(use-package emacs
+  :custom
+  ;; TAB cycle if there are only few candidates
+  (completion-cycle-threshold 3)
+
+  ;; Enable indentation+completion using the TAB key.
+  ;; `completion-at-point' is often bound to M-TAB.
+  (tab-always-indent 'complete)
+
+  ;; Emacs 30 and newer: Disable Ispell completion function.
+  ;; Use `cape-dict' as an alternative
+  (text-mode-ispell-word-completion nil)
+
+  ;; Hide commands in M-x which do not apply to the current mode.
+  ;; Corfu commands are hidden, since they are not used via M-x.
+  (read-extended-command-predicate #'command-completion-default-include-p))
+
+;; use-package
 (use-package use-package
   :ensure nil
   :custom use-package-always-ensure t)
@@ -168,6 +186,20 @@ source: https://www.emacswiki.org/emacs/FlySpell "
 (use-package which-key
   :config
   (which-key-mode))
+
+;; corfu.el (COmpletion in Region FUnction)
+(use-package corfu
+  :init (global-corfu-mode)
+  :custom
+  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  ;; (corfu-separator ?\s)          ;; Orderless field separator
+  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
+  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+  (corfu-auto t))
 
 ;; org-modern
 (use-package org-modern
@@ -222,15 +254,6 @@ source: https://www.emacswiki.org/emacs/FlySpell "
 
 ;; dape
 (use-package dape)
-
-;; company-mode
-(use-package company
-  :init
-  (add-hook 'after-init-hook 'global-company-mode)
-  :config
-  (setq company-minimum-prefix-length 1
-        company-idle-delay 0.0) ;; default: 0.2
-  (add-hook 'eshell-mode-hook (lambda () (company-mode -1)))) ; disable company in eshell
 
 ;; extra major modes
 (use-package racket-mode)
