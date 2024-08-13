bc1939a327cfc7653b9923c75586803efd5be177
Yiyu Zhou <yiyuzhou19@gmail.com>
2024-08-12 19:02:46 -0700

    Move some config to early-init.el

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -17,4 +17,21 @@
 ;; no-littering (keeping ~/.config/emacs clean)
 (use-package no-littering)
 
-(setq use-dialog-box nil)
+;; Emacs settings
+(use-package emacs
+  :after no-littering
+  :config
+  ;; hide uneeded UI elements
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+  (menu-bar-mode -1)
+
+  ;; disable dialog box
+  (setq use-dialog-box nil)
+
+  ;; inhibit startup message
+  (setq inhibit-startup-message t
+        visible-bell t)
+
+  ;; hide native comp warnings
+  (setq native-comp-async-report-warnings-errors nil))
