ea5dd39a1028c497d3537b6e9d5c0ba61275f5d6
Yiyu Zhou <yiyuzhou19@gmail.com>
2024-08-12 19:02:46 -0700

    Move some config to early-init.el

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,15 +1,3 @@
-;; inhibit startup message
-(setq inhibit-startup-message t
-      visible-bell t)
-
-;; hide uneeded UI elements
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(menu-bar-mode -1)
-
-;; hide native comp warnings
-(setq native-comp-async-report-warnings-errors nil)
-
 ;; enable line number
 (global-display-line-numbers-mode 1)
 
