69f50e39f4594fb68640d7b514deb56854611877
Yiyu Zhou <yiyuzhou19@gmail.com>
2025-09-11 16:58:47 -0700

    Clean up

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -8,17 +8,18 @@
 
 ;; hide uneeded UI elements
 (menu-bar-mode -1)
-(dolist (func '(tool-bar-mode scroll-bar-mode))    ; doesn't exist in TTY frames
+(dolist (func
+	 '(tool-bar-mode scroll-bar-mode)) ; nonexistent in TTY frames
   (when (fboundp func)
     (funcall func -1)))
 
 (setq-default
- use-dialog-box nil                                ; disable dialog box
+ use-dialog-box nil
  visible-bell t)
 
 (setopt
  warning-suppress-log-types '((comp) (bytecomp))
- native-comp-async-report-warnings-errors 'silent  ; disable native comp warning pop-up
- inhibit-startup-message t)                        ; inhibit startup message
+ native-comp-async-report-warnings-errors 'silent
+ inhibit-startup-message t)
 
 ;;; early-init.el ends here
